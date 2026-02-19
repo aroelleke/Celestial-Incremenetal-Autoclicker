a6667a9fc32674607bfa9d4a237cbb08b7e8a946
Adrian Rölleke <adrian.roelleke@gmail.com>
2026-02-19 14:30:19 +0100

    chor: add data

---
+++ data.js
@@ -0,0 +1,91 @@
+export const ClickElementIDs = [
+    // OR
+    "upgrade-i-11",
+    "upgrade-i-12",
+    "upgrade-i-13",
+    "upgrade-i-14",
+    "upgrade-i-15",
+    "upgrade-i-16",
+    "upgrade-i-17",
+    "upgrade-i-18",
+    "upgrade-i-19",
+    "upgrade-i-20",
+    "upgrade-i-21",
+    // Rank Main
+    "clickable-r-11",
+    "clickable-r-12",
+    "clickable-r-13",
+    // Rank Pent
+    "clickable-r-14",
+    // Prestige
+    "upgrade-p-11",
+    "upgrade-p-12",
+    "upgrade-p-13",
+    "upgrade-p-14",
+    "upgrade-p-15",
+    "upgrade-p-16",
+    "upgrade-p-17",
+    "upgrade-p-18",
+    "upgrade-p-19",
+    "upgrade-p-20",
+    "upgrade-p-21",
+    "upgrade-p-22",
+    "upgrade-p-23",
+    // Grass
+    "upgrade-g-11",
+    "upgrade-g-12",
+    "upgrade-g-13",
+    "upgrade-g-14",
+    "upgrade-g-15",
+    "upgrade-g-16",
+    "upgrade-g-17",
+    "upgrade-g-18",
+    "upgrade-g-19",
+    "upgrade-g-20",
+    // Grasshopper
+    "buyable-gh-11",
+    "buyable-gh-12",
+    "buyable-gh-13",
+    "buyable-gh-14",
+    "buyable-gh-15",
+    "buyable-gh-16",
+    "buyable-gh-17",
+    "buyable-gh-18",
+    "buyable-gh-19",
+    "buyable-gh-20",
+    // CB
+    "clickable-cb-11",
+    "clickable-cb-12",
+    "clickable-cb-13",
+    "clickable-cb-14",
+    "clickable-cb-15",
+    "clickable-cb-16",
+    "clickable-cb-17",
+    "clickable-cb-18",
+    "clickable-cb-101",
+    "clickable-cb-102",
+    "clickable-cb-103",
+    "clickable-cb-104",
+    "clickable-cb-105",
+    // Dice
+    "clickable-d-11",
+    // Antimatter
+    "clickable-ad-4",   //Max All
+    "buyable-ad-2",     // Dimension Boost
+    "buyable-ad-3",     // Antimatter Glaxies
+    // Infinity
+    "upgrade-ip-11",
+    "upgrade-ip-12",
+    "upgrade-ip-13",
+    "upgrade-ip-14",
+    "upgrade-ip-21",
+    "upgrade-ip-22",
+    "upgrade-ip-23",
+    "upgrade-ip-24",
+    "upgrade-ip-31",
+]
+
+export const HoverElementIDs = [
+    // Grass
+    "tile"
+]
