ade3d3e760f037b0ee5687c56b75bce23a42cc91
Adrian Rölleke <adrian.roelleke@gmail.com>
2026-02-19 15:14:12 +0100

    feat(Clicker): add elementsbyclass function

--- Clicker.js
+++ Clicker.js
@@ -1,15 +1,31 @@
 export class Clicker {
     DELAY = 50
     ELEMENT_IDS = []
+    ELEMENT_CLASS = []
     run = false
 
     constructor(options = {}) {
         this.ELEMENT_IDS = options.elementIDs
+        this.ELEMENT_CLASS = options.elementClasses
         this.DELAY = options.delay || this.DELAY
     }
 
     async start() {
         this.run = true
+
+        let elementsByClass = Array.from(document.getElementsByClassName(this.ELEMENT_CLASS))
+        let elementsByIds = this.ELEMENT_IDS.map((id) => document.getElementById(id))
+        
+        let elements = elementsByClass.concat(elementsByIds)
+
+        for (let i = 0; i < elements.length; i++) {
+            let element = elements.item(i)
+            try {
+                if (element instanceof HTMLElement) this.hover(element)
+            } catch (e) {
+                console.error(`Error hovering element ${element.id}:`, e)
+            }
+        }
 
         this.ELEMENT_IDS.forEach((id) => {
             let button = document.getElementById(id)
